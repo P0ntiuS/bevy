b29ce9e086f5b5e25383f3ad4e38724bf4bb4889
Maksim Kabanov <maksim_kabanov@epam.com>
2021-05-06 15:04:36 +0400

    enable drag_and_drop for windows

--- examples/app/drag_and_drop.rs
+++ examples/app/drag_and_drop.rs
@@ -2,7 +2,9 @@ use bevy::prelude::*;
 
 fn main() {
     App::build()
-        .add_plugins(DefaultPlugins)
+        .add_plugins_with(DefaultPlugins, |group| {
+            group.disable::<bevy::audio::AudioPlugin>()
+        })
         .add_system(file_drag_and_drop_system.system())
         .run();
 }
